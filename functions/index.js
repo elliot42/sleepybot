6207f8b0ca788f66df6490952ec780a1bb52e441
Elliot Block <elliot@framed.io>
2017-05-20 16:14:21 -0700

    Switch to call text coming from callXml webhook

--- functions/index.js
+++ functions/index.js
@@ -18,3 +18,8 @@ exports.config = functions.https.onRequest((request, response) => {
   console.log(request.body);
   response.send("Config not actually saved yet.");
 });
+
+exports.callXml = functions.https.onRequest((request, response) => {
+  console.log(request.body);
+	response.send("<Response><Say>Goodnight!</Say></Response>");
+});
